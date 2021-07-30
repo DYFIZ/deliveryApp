d24d9b6ff25649329ee2d306356f339fb624a424
mytsak ivan <mytsakivan@gmail.com>
2021-07-30 23:16:39 +0300

    main logic

--- app/controllers/couriers_controller.rb
+++ app/controllers/couriers_controller.rb
@@ -2,10 +2,44 @@ class CouriersController < ApplicationController
   def index
     @couriers = Courier.all
   end
+ 
+  def new
+    @courier = Courier.new
+    respond_to do |format|
+      format.html{render :new, locals: {courier: @courier}}
+    end  
+  end
+ 
   def show
-    @couriers = Courier.all
+    @courier = Courier.find(params[:id]) 
+    respond_to do |format|
+      format.html{render :show, locals: {courier: @courier}}
+    end  
   end
+
+
   def edit
-    @couriers = Courier.all
+    @courier = Courier.find(params[:id]) 
+    respond_to do |format|
+      format.html{render :edit, locals: {courier: @courier}}
+    end  
   end
+
+  def update
+    @courier = Courier.find(params[:id])
+    @courier.update(courier_params)
+
+    redirect_to show_path(@courier)
+  end
+
+  def create
+    courier = Courier.create(courier_params)
+    redirect_to couriers_index_path 
+  end  
+
+  private
+
+  def courier_params
+    params.require(:courier).permit(:name, :email);
+  end  
 end
