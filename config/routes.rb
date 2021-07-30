733d76f5ad3f197d76d71b2752d67c524fce266e
mytsak ivan <mytsakivan@gmail.com>
2021-07-30 10:06:32 +0300

    add a route for couriers/add view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'couriers/index'
+  get '/couriers/add', to: 'couriers#add', as: "add"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
