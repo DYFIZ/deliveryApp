ebdcf1ad4b877432ec17d1ef428bf087141f062b
mytsak ivan <mytsakivan@gmail.com>
2021-07-30 03:02:44 +0300

    add show and edit methods to controller couriers

--- app/controllers/couriers_controller.rb
+++ app/controllers/couriers_controller.rb
@@ -1,4 +1,11 @@
 class CouriersController < ApplicationController
   def index
+    @couriers = Courier.all
+  end
+  def show
+    @couriers = Courier.all
+  end
+  def edit
+    @couriers = Courier.all
   end
 end
