c65401d5e1fb2062691e3eaf50a1c560b293fb01
mytsak ivan <mytsakivan@gmail.com>
2021-07-30 23:11:20 +0300

    routers for  for CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get 'couriers/index'
-  get '/couriers/add', to: 'couriers#add', as: "add"
+  get 'couriers/new', to: 'couriers#new', as: "new"
+  get 'couriers/show/:id', to: 'couriers#show', as: "show"
+  
+  get 'couriers/edit/:id', to: 'couriers#edit', as: "edit"
+  post 'couriers/edit/:id', to: 'couriers#update', as: "update"
+  post 'couriers/new', to: 'couriers#create'
+  post 'couriers/update', to: 'couriers#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
