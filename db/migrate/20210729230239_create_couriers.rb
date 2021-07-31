7370a12a90a3ce6cc1be5d64ed30315f8416663f
mytsak ivan <mytsakivan@gmail.com>
2021-07-31 23:46:10 +0300

    added new field to make one to many relationship

--- db/migrate/20210729230239_create_couriers.rb
+++ db/migrate/20210729230239_create_couriers.rb
@@ -1,9 +1,9 @@
 class CreateCouriers < ActiveRecord::Migration[6.1]
   def change
-    create_table :couriers do |t|
+    create_table :couriers do |t| 
       t.string :name
       t.string :email
-
+      t.index :courier_id
       t.timestamps
     end
   end
