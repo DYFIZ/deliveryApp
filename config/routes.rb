e624b8a5fc5fb3c9973c72a4def36cf7142f565a
mytsak ivan <mytsakivan@gmail.com>
2021-07-31 23:48:15 +0300

    working with a packages routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
   get 'couriers/index'
   get 'couriers/new', to: 'couriers#new', as: "new"
   get 'couriers/show/:id', to: 'couriers#show', as: "show"
-  
   get 'couriers/edit/:id', to: 'couriers#edit', as: "edit"
   post 'couriers/edit/:id', to: 'couriers#update', as: "update"
   post 'couriers/new', to: 'couriers#create'
   post 'couriers/update', to: 'couriers#update'
+
+  
+  get 'packages/new/:id', to: 'packages#new', as: "delivery"
+  get 'packages/new/:i', to: 'packages#show', as: "delivery"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
